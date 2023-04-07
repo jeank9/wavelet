7082a6e783a4a135101a0617a36b06cac3487300
jeannie <jlk004@ucsd.edu>
2023-04-07 13:56:44 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -9,7 +9,7 @@ class Handler implements URLHandler {
     public String handleRequest(URI url) {
         System.out.println(url);
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Jeannie's Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
